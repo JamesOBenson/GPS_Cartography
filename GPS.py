88f1689731ee22e5b3441e06b4f3c88ae54bc4c8
James Benson <JamesOBenson@users.noreply.github.com>
2018-05-08 11:26:57 -0500

    Update GPS.py

--- GPS.py
+++ GPS.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+#
+# Copyright 2018 James Benson
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
 """
 This program requires python3 and a user to input a center latitude and a center longitude.
 It will then plot that point in blue on a google maps, along with a circular radius.
